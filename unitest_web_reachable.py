62c193e2d73a695997e14a8db0d0f353c4d553ac
osher.hacham <34osher@gmail.com>
2023-09-12 12:38:41 +0300

    uni localhost

--- unitest_web_reachable.py
+++ unitest_web_reachable.py
@@ -5,7 +5,7 @@ import requests
 class TestWebsiteReachability(unittest.TestCase):
 
     def test_website_reachable(self):
-        response = requests.get('http://172.31.40.84')
+        response = requests.get('http://127.0.0.1')
         self.assertEqual(response.status_code, 200)
 
 
