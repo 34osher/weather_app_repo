77fb3da265632ceea2f2456208d9770342ca2af9
osher.hacham <34osher@gmail.com>
2023-09-19 14:58:07 +0300

    chenge prune to -a

--- deploy_compose/deploy_script.sh
+++ deploy_compose/deploy_script.sh
@@ -5,6 +5,6 @@ echo "starting deployment"
 cd /home/ubuntu/deploy/
 
 docker compose down -v
-docker rmi -f "$(docker images -aq)"
+docker rmi -fa "$(docker images -aq)"
 
 docker compose up -d --scale flask_app=2
